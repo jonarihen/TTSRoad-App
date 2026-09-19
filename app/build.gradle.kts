1e373e1a74a69687892c214657adf5f56653cadb
jonarihen <154433893+jonarihen@users.noreply.github.com>
2026-09-19 22:04:50 +0000

    Prepare 0.16.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "dk.perspektiva.ttsroad"
         minSdk = 26
         targetSdk = 37
-        versionCode = 25
-        versionName = "0.15.7"
+        versionCode = 26
+        versionName = "0.16.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "SENTRY_DSN", "\"$ttsRoadSentryDsn\"")
     }
